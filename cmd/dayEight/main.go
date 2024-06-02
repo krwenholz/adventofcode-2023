9f4411b930e674f630ecf36f2abe182a779994ff
Kyle Wenholz <kyle@krwenholz.com>
2024-06-02 22:28:00 +0000

    Start a refactor to support new node name format

--- cmd/dayEight/main.go
+++ cmd/dayEight/main.go
@@ -1,69 +1,78 @@
 package dayEight
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"log/slog"
 	"os"
 
 	"github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
 	"github.com/spf13/cobra"
 )
 
-type ParsedInput struct {
-	Instructions string  `@Ident`
-	Nodes        []*Node `@@*`
-	MappedNodes  map[string]*Node
-}
-
 type Node struct {
-	Name  string `@Ident "="`
-	Left  string `"(" @Ident ","`
-	Right string `@Ident ")"`
+	Name  string `@Node " = " "("`
+	Left  string `@Node ", "`
+	Right string `@Node ")" EOL?`
 }
 
 func (n *Node) String() string {
 	return fmt.Sprintf("%s = (%s, %s)", n.Name, n.Left, n.Right)
 }
 
-func parse(puzzleFile string) *ParsedInput {
-	f, err := os.ReadFile(puzzleFile)
+func parse(path string) (string, map[string]*Node) {
+	f, err := os.Open(path)
 	if err != nil {
-		slog.Error("failed to parse", err)
-		panic(err)
+		log.Fatal(err)
 	}
 
-	parser, err := participle.Build[ParsedInput]()
+	scanner := bufio.NewScanner(f)
+
+	nodeLexer := lexer.MustSimple([]lexer.SimpleRule{
+		// Order matters here! Int kept stealing the leading cards before I changed the ordering.
+		{"Node", `[A-Z0-9]{3}`},
+		{"EOL", `\n`},
+		{"Colon", `:`},
+		{"Whitespace", `[ \t]+`},
+	})
+	parser, err := participle.Build[Node](
+		participle.Lexer(nodeLexer),
+		participle.Elide("Whitespace"),
+	)
 	if err != nil {
 		slog.Error("failed to parse", "err", err)
 		panic(err)
 	}
 
-	input, err := parser.ParseBytes("", f)
-	if err != nil {
-		slog.Error("failed to parse", "so far", input, "err", err)
-		panic(err)
+	instructions := scanner.Text()
+
+	nodes := map[string]*Node{}
+	for scanner.Scan() {
+		node, err := parser.ParseString(path, scanner.Text())
+		if err != nil {
+			slog.Error("failed to parse", "err", err)
+			panic(err)
+		}
+		nodes[node.Name] = node
 	}
 
-	input.MappedNodes = make(map[string]*Node)
-	for _, node := range input.Nodes {
-		input.MappedNodes[node.Name] = node
-	}
-
-	return input
+	return instructions, nodes
 }
 
 func partOne(puzzleFile string) {
-	input := parse(puzzleFile)
-	slog.Debug("parsed input", "input", input)
+	instructions, nodes := parse(puzzleFile)
+	slog.Debug("parsed input", "instructions", instructions, "nodes", nodes)
 
-	cur := input.MappedNodes["AAA"]
+	cur := nodes["AAA"]
 	steps := 0
 	for cur.Name != "ZZZ" {
-		switch input.Instructions[steps%len(input.Instructions)] {
+		switch instructions[steps%len(instructions)] {
 		case 'L':
-			cur = input.MappedNodes[cur.Left]
+			cur = nodes[cur.Left]
 		case 'R':
-			cur = input.MappedNodes[cur.Right]
+			cur = nodes[cur.Right]
 		}
 		steps++
 		slog.Debug("stepping", "cur", cur, "steps", steps)
@@ -72,7 +81,43 @@ func partOne(puzzleFile string) {
 }
 
 func partTwo(puzzleFile string) {
-	fmt.Println("Day part two", puzzleFile)
+	instructions, nodes := parse(puzzleFile)
+	slog.Debug("parsed input", "input", instructions, "nodes", nodes)
+
+	curs := []*Node{}
+	for _, node := range nodes {
+		if node.Name[2] == 'A' {
+			curs = append(curs, node)
+		}
+	}
+	steps := 0
+	for {
+		newCurs := []*Node{}
+		for _, cur := range curs {
+			switch instructions[steps%len(instructions)] {
+			case 'L':
+				cur = nodes[cur.Left]
+			case 'R':
+				cur = nodes[cur.Right]
+			}
+			newCurs = append(newCurs, cur)
+		}
+		curs = newCurs
+		steps++
+		slog.Debug("stepping", "curs", curs, "steps", steps)
+
+		allZs := true
+		for _, cur := range curs {
+			if cur.Name[2] != 'Z' {
+				allZs = false
+				break
+			}
+		}
+		if allZs {
+			break
+		}
+	}
+	slog.Info("Day eight part one", "steps", steps)
 }
 
 var Cmd = &cobra.Command{
