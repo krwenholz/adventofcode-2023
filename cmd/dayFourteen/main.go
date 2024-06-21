ba22e723ad825a405322912edda7ccb339c4bce1
Kyle Wenholz <kyle@krwenholk.com>
2024-06-21 20:53:18 +0000

    Slow version of day thirteen working

--- cmd/dayFourteen/main.go
+++ cmd/dayFourteen/main.go
@@ -2,7 +2,9 @@ package dayFourteen
 
 import (
 	"adventofcode/cmd/fileReader"
+	"fmt"
 	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -14,11 +16,7 @@ import (
 3. sum it for all rounded rocks
 **/
 
-func partOne(puzzleFile string) {
-	slog.Info("Day Fourteen part one", "puzzle file", puzzleFile)
-
-	rows := strings.Split(fileReader.ReadFileContents(puzzleFile), "\n")
-
+func load(rows []string) int {
 	colEnds := map[int]int{} // map the last row index for each column
 	for i := range rows[0] {
 		colEnds[i] = len(rows)
@@ -46,11 +44,142 @@ func partOne(puzzleFile string) {
 		}
 	}
 
-	slog.Info("Day fourteen part one total load", "load", loadTotal)
+	return loadTotal
 }
 
-func partTwo(puzzleFile string) {
+func partOne(puzzleFile string) {
+	slog.Info("Day Fourteen part one", "puzzle file", puzzleFile)
+
+	rows := strings.Split(fileReader.ReadFileContents(puzzleFile), "\n")
+
+	slog.Info("Day fourteen part one total load", "load", load(rows))
+}
+
+func replace(row string, col int, char byte) string {
+	return row[:col] + string(char) + row[col+1:]
+}
+
+func tiltNorth(col int, rows []string) []string {
+	stopRow := 0
+	for rowI := range rows {
+		row := rows[rowI]
+		switch row[col] {
+		case '.':
+			// keep going
+		case '#':
+			// new stop
+			stopRow = rowI + 1
+		case 'O':
+			// roll!
+			rows[rowI] = replace(row, col, '.')
+			rows[stopRow] = replace(rows[stopRow], col, 'O')
+			stopRow = stopRow + 1
+		}
+	}
+	return rows
+}
+
+func tiltSouth(col int, rows []string) []string {
+	stopRow := len(rows) - 1
+	for rowI := len(rows) - 1; rowI >= 0; rowI-- {
+		row := rows[rowI]
+		switch row[col] {
+		case '.':
+			// keep going
+		case '#':
+			// new stop
+			stopRow = rowI - 1
+		case 'O':
+			// roll!
+			rows[rowI] = replace(row, col, '.')
+			rows[stopRow] = replace(rows[stopRow], col, 'O')
+			stopRow = stopRow - 1
+		}
+	}
+
+	return rows
+}
+
+func tiltWest(row int, rows []string) []string {
+	stopCol := 0
+	for col := range rows[0] {
+		switch rows[row][col] {
+		case '.':
+			// keep going
+		case '#':
+			// new stop
+			stopCol = col + 1
+		case 'O':
+			// roll!
+			rows[row] = replace(rows[row], col, '.')
+			rows[row] = replace(rows[row], stopCol, 'O')
+			stopCol = stopCol + 1
+		}
+	}
+	return rows
+}
+
+func tiltEast(row int, rows []string) []string {
+	stopCol := len(rows[0]) - 1
+	for col := len(rows[0]) - 1; col >= 0; col-- {
+		switch rows[row][col] {
+		case '.':
+			// keep going
+		case '#':
+			// new stop
+			stopCol = col - 1
+		case 'O':
+			// roll!
+			rows[row] = replace(rows[row], col, '.')
+			rows[row] = replace(rows[row], stopCol, 'O')
+			stopCol = stopCol - 1
+		}
+	}
+	return rows
+}
+
+func spinCycle(rows []string) []string {
+	for i := range rows[0] {
+		rows = tiltNorth(i, rows)
+	}
+	if os.Getenv("LOG_TILTS") == "YES" {
+		fmt.Print("tilted North\n", strings.Join(rows, "\n"), "\n")
+	}
+	for i := range rows {
+		rows = tiltWest(i, rows)
+	}
+	if os.Getenv("LOG_TILTS") == "YES" {
+		fmt.Print("tilted West\n", strings.Join(rows, "\n"), "\n")
+	}
+	for i := range rows[0] {
+		rows = tiltSouth(i, rows)
+	}
+	if os.Getenv("LOG_TILTS") == "YES" {
+		fmt.Print("tilted South\n", strings.Join(rows, "\n"), "\n")
+	}
+	for i := range rows {
+		rows = tiltEast(i, rows)
+	}
+	if os.Getenv("LOG_TILTS") == "YES" {
+		fmt.Print("tilted East\n", strings.Join(rows, "\n"), "\n")
+	}
+
+	if os.Getenv("LOG_CYCLES") == "YES" {
+		fmt.Print("cycle\n", strings.Join(rows, "\n"), "\n")
+	}
+
+	return rows
+}
+
+func partTwo(puzzleFile string, cycles int) {
 	slog.Info("Day Fourteen part two", "puzzle file", puzzleFile)
+
+	rows := strings.Split(fileReader.ReadFileContents(puzzleFile), "\n")
+	for i := 0; i < cycles; i++ {
+		spinCycle(rows)
+	}
+
+	slog.Info("Day fourteen part two total load", "load", load(rows))
 }
 
 var Cmd = &cobra.Command{
@@ -60,11 +189,13 @@ var Cmd = &cobra.Command{
 		if !cmd.Flag("part-two").Changed {
 			partOne(puzzleInput)
 		} else {
-			partTwo(puzzleInput)
+			cycles, _ := cmd.Flags().GetInt("cycles")
+			partTwo(puzzleInput, cycles)
 		}
 	},
 }
 
 func init() {
 	Cmd.Flags().Bool("part-two", false, "Whether to run part two of the day's challenge")
+	Cmd.Flags().Int("cycles", 1, "Cycles to run, only applicable for part two")
 }
