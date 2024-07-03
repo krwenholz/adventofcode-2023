6a4183e471d491d62c90e777df22fba319890d6a
Kyle Wenholz <kyle@krwenholk.com>
2024-07-03 22:34:25 +0000

    Probably working day eighteen but way too slow part 2

--- cmd/dayEighteen/main.go
+++ cmd/dayEighteen/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/alecthomas/participle/v2"
@@ -216,6 +217,131 @@ func partOne(puzzleFile string) {
 
 func partTwo(puzzleFile string) {
 	slog.Info("Day Eighteen part two", "puzzle file", puzzleFile)
+
+	commands := parseCommands(puzzleFile)
+
+	maxX, maxY, minX, minY := 0, 0, 0, 0
+	grid := map[int]map[int]*Space{}
+	pos := &Coordinate{0, 0}
+	for cI, bustedCommand := range commands {
+		dist, _ := strconv.ParseInt(bustedCommand.Color[:5], 16, 32)
+		var dir string
+		switch bustedCommand.Color[5:] {
+		case "0":
+			dir = "R"
+		case "1":
+			dir = "D"
+		case "2":
+			dir = "L"
+		case "3":
+			dir = "U"
+		}
+
+		c := &DigCommand{dir, int(dist), bustedCommand.Color}
+		slog.Debug("fixed command", "bustedCommand", bustedCommand, "c", c)
+
+		for i := 0; i < c.Dist; i++ {
+			switch c.Dir {
+			case "R":
+				pos.Col++
+			case "L":
+				pos.Col--
+			case "U":
+				pos.Row++
+			case "D":
+				pos.Row--
+			}
+
+			if pos.Col > maxX {
+				maxX = pos.Col
+			}
+			if pos.Col < minX {
+				minX = pos.Col
+			}
+			if pos.Row > maxY {
+				maxY = pos.Row
+			}
+			if pos.Row < minY {
+				minY = pos.Row
+			}
+
+			if grid[pos.Row] == nil {
+				grid[pos.Row] = map[int]*Space{}
+			}
+
+			var shape string
+			if i == c.Dist-1 {
+				shape = cornerShape(c, commands[(cI+1)%len(commands)])
+			} else {
+				switch c.Dir {
+				case "R":
+					shape = "-"
+				case "L":
+					shape = "-"
+				case "U":
+					shape = "|"
+				case "D":
+					shape = "|"
+				}
+			}
+
+			grid[pos.Row][pos.Col] = &Space{shape, c.Color}
+		}
+	}
+
+	slog.Debug("Finished building grid", "size", len(grid), "width", maxX-minX, "height", maxY-minY)
+
+	filledPositions := 0
+	//printGrid := &strings.Builder{}
+	printGrid := &NoopStringBuilder{}
+	printGrid.Grow(maxY - minY + maxX - minX)
+	for row := maxY; row >= minY; row-- {
+		filledThisRow := 0
+		wallCount := 0
+		for col := minX; col <= maxX; col++ {
+			if s, ok := grid[row][col]; ok {
+				filledThisRow++
+				printGrid.WriteString(s.Shape)
+				switch s.Shape {
+				case "|":
+					wallCount++
+				case "L":
+					wallCount++
+				case "J":
+					wallCount++
+				}
+			} else {
+				if wallCount%2 == 1 {
+					filledThisRow++
+				}
+				printGrid.WriteString(".")
+			}
+		}
+
+		filledPositions += filledThisRow
+
+		printGrid.WriteString(fmt.Sprintf(" (%d) \n", filledThisRow))
+
+		if row%500 == 0 {
+			slog.Debug("row filling", "row", row, "percent", float64((maxY-row)*100)/float64((maxY-minY)))
+		}
+	}
+
+	slog.Info("finished digging", "filled positions", filledPositions)
+
+	os.WriteFile("/tmp/dayEighteenGrid.txt", []byte(printGrid.String()), 0644)
+}
+
+type NoopStringBuilder struct{}
+
+func (sb *NoopStringBuilder) WriteString(p string) (n int, err error) {
+	return len(p), nil
+}
+
+func (sb *NoopStringBuilder) Grow(i int) {}
+
+func (sb *NoopStringBuilder) String() string {
+	return ""
 }
 
 var Cmd = &cobra.Command{
