ce9aca7331237f99223990b1455e187c0576086a
Kyle Wenholz <kyle@krwenholk.com>
2024-07-03 18:51:47 +0000

    Day seventeen part two is done

--- cmd/daySeventeen/main.go
+++ cmd/daySeventeen/main.go
@@ -66,25 +66,6 @@ func (c *Cell) String() string {
 	return fmt.Sprintf("Cell{%s, dir: %s, steps: %d, f: %d, g: %d, h: %d}", c.coords.String(), c.dir, c.steps, c.f, c.g, c.h)
 }
 
-func (c *Cell) Directions() []*Direction {
-	if c.dir == nil {
-		// Start point, only left and "down" are valid
-		return []*Direction{
-			{0, 1},
-			{1, 0},
-		}
-	}
-	dirs := []*Direction{
-		// Nifty transform for turns
-		{-c.dir.Col, c.dir.Row},
-		{c.dir.Col, -c.dir.Row},
-	}
-	if c.steps < 3 {
-		dirs = append(dirs, c.dir)
-	}
-	return dirs
-}
-
 func (c *Cell) Next(dir *Direction, dest *Coordinate, grid [][]int) (*Cell, error) {
 	var steps int
 
@@ -140,7 +121,11 @@ func HeatLoss(row, col int, grid []string) int {
 }
 
 // An A* implementation!
-func AStarSearch(grid [][]int, src, dest *Coordinate) ([]*Cell, int, [][]int) {
+func AStarSearch(grid [][]int,
+	src, dest *Coordinate,
+	dirsFunc func(c *Cell) []*Direction,
+	finished func(c *Cell, d *Coordinate) bool,
+) ([]*Cell, int, [][]int) {
 	// Initialize the closed list (visited cells)
 	seen := map[string]*Cell{}
 	// Track the best paths
@@ -164,14 +149,14 @@ func AStarSearch(grid [][]int, src, dest *Coordinate) ([]*Cell, int, [][]int) {
 	for len(*openSet) > 0 {
 		current := heap.Pop(openSet).(*Cell)
 
-		if current.coords.Equals(dest) {
+		if finished(current, dest) {
 			return ReconstructPath(cameFrom, current), current.f, gScore
 		}
 
 		slog.Debug("popped!", "cell", current, "open list len", len(*openSet))
 
 		// For each direction, check the successors
-		for _, dir := range current.Directions() {
+		for _, dir := range dirsFunc(current) {
 			neighbor, err := current.Next(dir, dest, grid)
 			if err != nil {
 				slog.Debug("invalid state", "cell", current, "dir", dir, "error", err)
@@ -230,6 +215,25 @@ func PrintCellDetails(cellDetails [][]int) {
 	os.WriteFile("/tmp/cells.txt", []byte(out), 0644)
 }
 
+func Directions(c *Cell) []*Direction {
+	if c.dir == nil {
+		// Start point, only left and "down" are valid
+		return []*Direction{
+			{0, 1},
+			{1, 0},
+		}
+	}
+	dirs := []*Direction{
+		// Nifty transform for turns
+		{-c.dir.Col, c.dir.Row},
+		{c.dir.Col, -c.dir.Row},
+	}
+	if c.steps < 3 {
+		dirs = append(dirs, c.dir)
+	}
+	return dirs
+}
+
 func partOne(puzzleFile string) {
 	slog.Info("Day Seventeen part one", "puzzle file", puzzleFile)
 	rows := strings.Split(fileReader.ReadFileContents(puzzleFile), "\n")
@@ -245,7 +249,9 @@ func partOne(puzzleFile string) {
 	src := &Coordinate{0, 0}
 	dest := &Coordinate{len(rows) - 1, len(rows[0]) - 1}
 
-	path, heatLoss, gScore := AStarSearch(grid, src, dest)
+	path, heatLoss, gScore := AStarSearch(grid, src, dest, Directions, func(c *Cell, d *Coordinate) bool {
+		return c.coords.Equals(d)
+	})
 
 	PrintPath(path, rows)
 	PrintCellDetails(gScore)
@@ -253,8 +259,61 @@ func partOne(puzzleFile string) {
 	slog.Info("The path from source to destination found", "path", path, "heat loss", heatLoss)
 }
 
+func UltraDirections(c *Cell) []*Direction {
+	if c.dir == nil {
+		// Start point, only left and "down" are valid
+		return []*Direction{
+			{0, 1},
+			{1, 0},
+		}
+	}
+
+	/**
+	Once an ultra crucible starts moving in a direction, it needs to move a minimum of four
+	blocks in that direction before it can turn (or even before it can stop at the end).
+	However, it will eventually start to get wobbly: an ultra crucible can move a maximum of
+	ten consecutive blocks without turning.
+	**/
+	if c.steps < 4 {
+		return []*Direction{c.dir}
+	}
+
+	dirs := []*Direction{
+		// Nifty transform for turns
+		{-c.dir.Col, c.dir.Row},
+		{c.dir.Col, -c.dir.Row},
+	}
+	if c.steps < 10 {
+		dirs = append(dirs, c.dir)
+	}
+	return dirs
+}
+
+func UltraFinished(c *Cell, d *Coordinate) bool {
+	return c.coords.Equals(d) && c.steps >= 4
+}
+
 func partTwo(puzzleFile string) {
 	slog.Info("Day Seventeen part two", "puzzle file", puzzleFile)
+	rows := strings.Split(fileReader.ReadFileContents(puzzleFile), "\n")
+	grid := make([][]int, len(rows))
+	for i, row := range rows {
+		grid[i] = make([]int, len(row))
+		for j, cell := range row {
+			h, _ := strconv.Atoi(string(cell))
+			grid[i][j] = h
+		}
+	}
+
+	src := &Coordinate{0, 0}
+	dest := &Coordinate{len(rows) - 1, len(rows[0]) - 1}
+
+	path, heatLoss, gScore := AStarSearch(grid, src, dest, UltraDirections, UltraFinished)
+
+	PrintPath(path, rows)
+	PrintCellDetails(gScore)
+
+	slog.Info("The path from source to destination found", "path", path, "heat loss", heatLoss)
 }
 
 var Cmd = &cobra.Command{
